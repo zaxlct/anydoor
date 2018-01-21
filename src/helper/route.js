5c48c50bbf7e924f5d054f3ad7206a4c268a0dbf
zaxlct <zaxlct@foxmail.com>
2018-01-21 13:37:12 +0800

    添加模板，更友好的展示文件名

--- src/helper/route.js
+++ src/helper/route.js
@@ -1,7 +1,16 @@
 const fs = require('fs')
+const path = require('path')
+const Handlebars = require('handlebars')
 const promisify = require('util').promisify
+
 const stat = promisify(fs.stat)
 const readdir = promisify(fs.readdir)
+const config = require('../config/defaultConfig')
+const mime = require('../helper/mime')
+
+const tplpath = path.join(__dirname, '../template/dir.tpl')
+const source = fs.readFileSync(tplpath)
+const template = Handlebars.compile(source.toString())
 
 module.exports = async (req, res, filePath) => {
   const stats = await stat(filePath).catch(ex => {
@@ -10,14 +19,22 @@ module.exports = async (req, res, filePath) => {
 		res.end(`${filePath} is not a directory or file`)
   })
   res.statusCode = 200
-  res.setHeader('Content-Type', 'text/plain')
   if (stats.isFile()) {
     // fs.readFile(filePath, (err, data) => {
     //   res.end(data)
     // })
+    const contentType = mime(filePath)
+    res.setHeader('Content-Type', contentType)
     fs.createReadStream(filePath).pipe(res)
   } else if (stats.isDirectory()) {
     const files = await readdir(filePath)
-    res.end(files.join(','))
+    const dir = path.relative(config.root, filePath)
+    const data = {
+      files,
+      dir: dir && `/${dir}`,
+      title: path.basename(filePath),
+    }
+    res.setHeader('Content-Type', 'text/html')
+    res.end(template(data))
   }
 }
